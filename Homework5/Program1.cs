a445eac0ae2252632c54a9f07fbef7222dff9b68
scrollme346 <133518123+scrollme346@users.noreply.github.com>
2023-11-10 17:09:42 +0200

    Program1.cs

--- Homework5/Program1.cs
+++ Homework5/Program1.cs
@@ -4,81 +4,69 @@ class Program
 {
     static void Main()
     {
-        int start, end;
-        try
-        {
-            while (true)//бесконечный цикл для коректного ввода
-            {
-                
-                Console.WriteLine("Введите первое число диапазона:");
-                if (int.TryParse(Console.ReadLine(), out start))
-                {
-                    break; // Прерывание цикла при успешном вводе числа
-                }
-                else
-                {
-                    Console.WriteLine("Введите корректное число.");
-                }
-                
-            }
+        int start = 0, end = 0;
 
-            while (true)
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Введите первое число диапазона:");
+                start = Convert.ToInt32(Console.ReadLine());
+                break; // выход из цикла только при корректном вводе
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введите корректное число.");
+            }
+        }
+
+        while (true)
+        {
+            try
             {
                 Console.WriteLine("Введите последнее число диапазона:");
-                if (int.TryParse(Console.ReadLine(), out end))
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Введите корректное число.");
-                }
-                
+                end = Convert.ToInt32(Console.ReadLine());
+                break; // выход из цикла только при корректном вводе
             }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введите корректное число.");
+            }
+        }
 
-            Console.WriteLine($"Все числа в диапазоне от {start} до {end}:");
-            for (int i = start; i <= end; i++)//условия при которых цикл завершится
+        Console.WriteLine($"Все числа в диапазоне:");
+        for (int i = start; i <= end; i++)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine("\n");
+
+        Console.WriteLine($"Числа в убывающем порядке:");
+        for (int i = end; i >= start; i--)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine("\n");
+
+        Console.WriteLine($"Числа кратные 7:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 7 == 0)
             {
                 Console.Write(i + " ");
             }
-            Console.WriteLine("\n");
-
-            Console.WriteLine($"Числа в убывающем порядке:");
-            for (int i = end; i >= start; i--)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine("\n");
-
-            Console.WriteLine($"Числа кратные 7:");
-            for (int i = start; i <= end; i++)
-            {
-                if (i % 7 == 0)
-                {
-                    Console.Write(i + " ");
-                }
-            }
-            Console.WriteLine("\n");
-
-            int count5 = 0;
-            for (int i = start; i <= end; i++)
-            {
-                if (i % 5 == 0)
-                {
-                    count5++;
-                }
-            }
-            Console.WriteLine($"Количество чисел кратных 5: {count5}");
         }
-        catch (OverflowException)
+        Console.WriteLine("\n");
+
+        int count5 = 0;
+        for (int i = start; i <= end; i++)
         {
-            Console.WriteLine("Результат операции слишком большой для данного типа данных");
+            if (i % 5 == 0)
+            {
+                count5++;
+            }
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Произошла непредвиденная ошибка: " + ex.Message);
-        }
-
-
+        Console.WriteLine($"Количество чисел кратных 5: {count5}");
     }
 }
+
